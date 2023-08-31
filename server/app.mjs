a33d5639ecfde9b915448526e7b8c49b966b6432
JahongLiu <jahongliu@gmail.com>
2023-08-30 20:54:35 -0400

    connect database

--- server/app.mjs
+++ server/app.mjs
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/timelapse-spot-v2");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "database connection error:"));
+db.once("open", () => {
+  console.log("Mongo databse connected");
+});
 
 app.get("/", (req, res) => {
   res.send("HELLO FROM JAHONG!");
